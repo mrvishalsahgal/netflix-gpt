067512817e69f3a4e995d33706ff4879c6cc5edf
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-01-26 09:19:10 +0530

    feat: talwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600 text-center mt-10">Netflix Clone</h1>
+    </>
+  )
+}
+
+export default App
